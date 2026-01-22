39cc6246fceb535cc4f1cc134aac35c4090d73d5
kuczkoowska <jagoda.kucz@wp.pl>
2026-01-22 19:51:48 +0100

    edit topic entity

--- src/models/Topic.js
+++ src/models/Topic.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
+const User = require("./User");
 
 const topicSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      required: [true, "Temat musi mieć nazwę"],
+      trim: true,
+      unique: true,
+      maxLength: 30,
     },
     description: {
       type: String,
+      required: [true, "Opis nie może być pusty"],
     },
     parent: {
       type: mongoose.Schema.Types.ObjectId,
@@ -27,14 +32,19 @@ const topicSchema = new mongoose.Schema(
     ],
     moderators: [
       {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        promotedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        promotedAt: { type: Date, default: Date.now },
       },
     ],
     blockedUsers: [
       {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        blockedAt: { type: Date, default: Date.now },
+        reason: String,
+        allowedSubtopics: [
+          { type: mongoose.Schema.Types.ObjectId, ref: "Topic" },
+        ],
       },
     ],
     isClosed: {
@@ -46,7 +56,7 @@ const topicSchema = new mongoose.Schema(
       default: false,
     },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 module.exports = mongoose.model("Topic", topicSchema);
